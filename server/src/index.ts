430f841f1fef965e0adf53714fe1648ad91c3484
sammaste <sam.lumi.estrella@gmail.com>
2025-03-28 18:03:01 +1000

    server modifications

--- server/src/index.ts
+++ server/src/index.ts
@@ -35,7 +35,7 @@ app.use("/teams", teamRoutes);
 /**
  * SERVER
  */
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on part ${port}`);
 });
